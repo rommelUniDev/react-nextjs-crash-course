6cf01dc3ee47c26d7605b9594a56ccce374933d2
Uni-Tech Admin <157199316+UniTechAdmin@users.noreply.github.com>
2024-03-05 17:17:25 +0800

    :construction: Scaffolding for baseline template and chapter 1

---
+++ chapters/ch-00/src/App.jsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div>
+      <h1>My First React App</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ chapters/ch-01/src/App.jsx
@@ -0,0 +1,12 @@
+import { Scoreboard } from './components/Scoreboard';
+
+export function App() {
+  return (
+    <div className='app'>
+      <h1>My First React App</h1>
+      <Scoreboard />
+    </div>
+  )
+}
+
+export default App;
