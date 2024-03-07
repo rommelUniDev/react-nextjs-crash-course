14a913c8fe8dade1948c184c8aa6d60f66dcc3c1
Uni-Tech Admin <157199316+UniTechAdmin@users.noreply.github.com>
2024-03-07 15:34:58 +0800

    :construction: Scaffolding for Chapter 2

---
+++ chapters/ch-02/src/components/Scoreboard.jsx
@@ -0,0 +1,27 @@
+import './Scoreboard.css';
+
+export function Scoreboard() {
+  return (
+    <>
+      <h1>Scoreboard</h1>
+      <div className='scoreboard'>
+        <div className='player-score'>
+          <h2>Player 1</h2>
+          <p className='score'>0</p>
+          <div className='buttons'>
+            <button className='button'>+</button>
+            <button className='button'>-</button>
+          </div>
+        </div>
+        <div className='player-score'>
+          <h2>Player 2</h2>
+          <p className='score'>0</p>
+          <div className='buttons'>
+            <button className='button'>+</button>
+            <button className='button'>-</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
