f449debdd6dc071c12879a46a48fde42d9b9a1a9
Uni-Tech Admin <157199316+UniTechAdmin@users.noreply.github.com>
2024-04-05 13:15:37 +0800

    :lipstick: Formatting

--- chapters/nj-final/src/app/page.jsx
+++ chapters/nj-final/src/app/page.jsx
@@ -1,12 +1,12 @@
-import { PokemonList } from "../components/PokemonList/PokemonList";
-import "./page.css";
+import { PokemonList } from '../components/PokemonList/PokemonList';
+import './page.css';
 
 const getFavoritePokemonData = async () => {
   const pokemonIds = [1, 4, 7, 25, 133, 150];
   const pokemons = await Promise.all(
     pokemonIds.map(async (id) => {
       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-      if (!res.ok) throw new Error("Failed to fetch data");
+      if (!res.ok) throw new Error('Failed to fetch data');
       return res.json();
     })
   );
@@ -17,7 +17,7 @@ export default async function Home() {
   const pokemons = await getFavoritePokemonData();
   return (
     <main>
-      <div className="pokemon-app">
+      <div className='pokemon-app'>
         <h1>My Pokemon App</h1>
         <h2>My Current Team</h2>
         <PokemonList pokemons={pokemons} />
